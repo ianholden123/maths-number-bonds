fb95b6e8976d706652b0c7e8bc8567e5b0296cad
Ian Holden <ianholden123@hotmail.co.uk>
2019-08-19 19:44:03 +0100

    Fixed grid numbers.

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -9,17 +9,16 @@ class Grid extends Component {
   render() {
     const output = []
     const cells = cellGenerator.generateCellsFromPhases(phases)
-    console.log('THIS IS SOMETHING', cells)
 
     for (let y = 0; y < cells.length; y++) {
-      for (let x = 0; x < cells[y].length + 2; x++) {
-        console.log('THIS IS Y', y, 'THIS IS X', x)
+      for (let x = 0; x < cells[y].length; x++) {
+        
         output.push(
-          <Cell key={`${y - 1},${x - 1}`} x={x - 1} y={y - 1}>
-            { x === 0 && y === 0 ? '+' : '' }
-            { x === 0 && y !== 0 ? y - 1 : '' }
-            { y === 0 && x !== 0 ? x - 1 : '' }
-            { x > 0 && y > 0 ? `${y - 1} + ${x - 1}` : '' }
+          <Cell key={`${y},${x}`} x={x} y={y}>
+            { x === -1 && y === -1 ? '+' : '' }
+            { x === -1 && y !== -1 ? y : '' }
+            { y === -1 && x !== -1 ? x : '' }
+            { x > -1 && y > -1 ? `${y} + ${x}` : '' }
           </Cell>
         )
       }
--- src/helpers/cellGenerator.js
+++ src/helpers/cellGenerator.js
@@ -3,28 +3,56 @@ let cellGenerator = {}
 cellGenerator.generateCellsFromPhases = (phases) => {
     if (!phases || typeof phases !== 'object' || phases.length === 0) return null
 
-    let cells = [[],[]]
+    let cells = []
+    const yAxisValue = cellGenerator.getAxisRange(phases, 'y')
 
-    phases.forEach(() => {
-      // To start a new phase, we should initialise an empty array that we can populate
+    for (let i = 0; i <= yAxisValue.maxAxisValue; i++) {
       cells.push([])
-    })
+    }
 
     phases.forEach(phase => {
       if (!phase.bonds || typeof phase.bonds !== 'object' || phase.bonds.length === 0) return null
 
       // Here we can populate each phase array with the relevant number bond information
       phase.bonds.forEach(bond => {
-        if (!bond || !bond.x || !bond.y) return null
+        if (!bond || !bond.hasOwnProperty('x') || !bond.hasOwnProperty('y')) return null
 
-        cells[bond.y -1][bond.x -1] = cellGenerator._createCell(bond.x, bond.y, phase)
-        cells[bond.x -1][bond.y -1] = cellGenerator._createCell(bond.y, bond.x, phase)
+        cells[bond.y][bond.x] = cellGenerator._createCell(bond.x, bond.y, phase)
+        cells[bond.x][bond.y] = cellGenerator._createCell(bond.y, bond.x, phase)
       })
     })
 
     return cells
 }
 
+/**
+ * Finds the largest and smallest values in a phases object so that we know what our axis value range
+ * should look like.
+ * @param phases - A phases object.
+ * @param axis - A string containing 'x' or 'y'.
+ */
+cellGenerator.getAxisRange = (phases, axis) => {
+  if (!phases || typeof phases !== 'object' || phases.length === 0) return null
+  if (axis !== 'x' && axis !== 'y') return null
+
+  let minAxisValue = Number.MAX_SAFE_INTEGER
+  let maxAxisValue = 0
+  phases.forEach((phase) => {
+    if (!phase.bonds || typeof phase.bonds !== 'object' || phase.bonds.length === 0) return null
+    
+    phase.bonds.forEach(bond => {
+      if (!bond || !('x' in bond) || !('y' in bond)) return null
+      if (bond[axis] < minAxisValue) minAxisValue = bond[axis]
+      if (bond[axis] > maxAxisValue) maxAxisValue = bond[axis]
+    })
+  })
+
+  return { minAxisValue, maxAxisValue }
+}
+
+/**
+ * Returns the object that should populate a cell component
+ */
 cellGenerator._createCell = (value1, value2, phase) => {
   return {
     x : value1,
