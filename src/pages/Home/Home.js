21a5360825c9185d0e22e6cd8bfa06cb2f17b703
ianholden123 <ianholden123@hotmail.co.uk>
2020-08-21 14:19:41 +0100

    Adjusted default "quick answer time" in config. Also hidden Practice Zone button on home page.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Progression in Number Bonds Within 20</h1>
-        <Link to="/practice-zone/"><button>Practice Zone</button></Link>
+        {/* <Link to="/practice-zone/"><button>Practice Zone</button></Link> */}
         <Link to="/assessment/"><button>Assessment</button></Link>
       </div>
     );
