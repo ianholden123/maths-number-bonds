3da1d67fe7241f0551a36590551ee82c1f71b1c0
ianholden123 <ianholden123@hotmail.co.uk>
2020-08-21 14:19:41 +0100

    Adjusted default "quick answer time" in config. Also hidden Practice Zone button on home page.

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,3 +1,3 @@
 // When an answer to a question has been given in less than this time (in
 // milliseconds), we deem this answer to be a 'quick answer'
-export const quickAnswerTime = 2000
+export const quickAnswerTime = 5000
