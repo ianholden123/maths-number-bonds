a1376e5f9c630328ec00b5bed50b069547f66a55
ianholden123 <ianholden123@hotmail.co.uk>
2020-08-27 10:48:27 +0100

    Improved layout on print preview pages. Fixed missing key icon.

--- src/components/Keys/Answers.js
+++ src/components/Keys/Answers.js
@@ -8,7 +8,12 @@ const AnswersKey = () => (
     <ul>
       <li className='correct'>Correctly</li>
       <li className='incorrect'>Incorrectly</li>
-      <li className='line'>{`In less than ${quickAnswerTime / 1000} ${quickAnswerTime / 1000 === 1 ? 'second' : 'seconds'}`}</li>
+      <li className='line'>
+        <div className='box'>
+          <svg id='strikethrough'><line x1="0" y1="0" x2="100%" y2="100%" strokeWidth='4' /></svg>
+        </div>
+        <span className='boxText'>{`In less than ${quickAnswerTime / 1000} ${quickAnswerTime / 1000 === 1 ? 'second' : 'seconds'}`}</span>
+      </li>
     </ul>
   </div>
 )
