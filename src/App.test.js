87e78d4f74c611740cc0b726d56cc3d201894646
ianholden123 <ianholden123@hotmail.co.uk>
2020-09-08 16:22:59 +0100

    Added some Enzyme tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
+import Home from './pages/Home/Home';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('<App />', () => {
+  it('renders the <Home /> page.', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Home)).toHaveLength(1);
+  })
+})
