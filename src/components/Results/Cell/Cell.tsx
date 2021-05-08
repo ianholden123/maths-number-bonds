e4e360373790a9149bfeeb80b8652a55c0e87ef8
Ian Holden <ianholden123@hotmail.co.uk>
2021-05-08 16:20:07 +0100

    Fixed some Typescript errors.

--- src/components/Results/Cell/Cell.tsx
+++ src/components/Results/Cell/Cell.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
+// import PropTypes from 'prop-types'; 
 import './Cell.css';
 import presentationHelper from '../../../helpers/presentation'
 
